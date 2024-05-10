cac14d275aba00dcb503015d78ba505c94430436
Shresthikashyap <122147931+Shresthikashyap@users.noreply.github.com>
2024-05-10 13:19:04 +0530

    Update new-group.js

--- public/js/new-group.js
+++ public/js/new-group.js
@@ -25,7 +25,7 @@ const newGroup = async(event) =>{
         const decodedToken = await parseJwt(token);
         const userId = decodedToken.id
 
-        const response = await axios.post(`http://23.20.253.19:3000/group/new-group/${userId}`,newGroupDetails,{
+        const response = await axios.post(`http://54.161.13.80:3000/group/new-group/${userId}`,newGroupDetails,{
             headers:{'Authorization':token}
         });
 
@@ -33,7 +33,7 @@ const newGroup = async(event) =>{
         localStorage.setItem('groupid',response.data.newGroupDetails.id);
         localStorage.setItem('groupName',response.data.newGroupDetails.groupName);
         const groupId = response.data.newGroupDetails.id;
-        localStorage.setItem('link',`http://23.20.253.19:3000/signup.html?groupId=${groupId}`) // group link to share 
+        localStorage.setItem('link',`http://54.161.13.80:3000/signup.html?groupId=${groupId}`) // group link to share 
         window.location.href = `group-chat.html?groupId=${groupId}`;
     }
     catch(error){
