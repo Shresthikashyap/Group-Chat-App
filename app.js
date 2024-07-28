ed09ce2563c3d70f0434b1f42d43f89b8a760c89
Shresthikashyap <122147931+Shresthikashyap@users.noreply.github.com>
2024-07-28 17:33:45 +0530

    Update app.js

--- app.js
+++ app.js
@@ -60,9 +60,11 @@ Group.hasMany(GroupFiles);
 User.hasMany(ArchievedMessage);
 Group.hasMany(ArchievedMessage);
 
+const port = process.env.PORT || 3000;
+
 sequelize.sync()   // is a way to sync your sequelize model with your database table
 .then(()=>{        // force: true => recreate the database table , drop the existing ones
-    server.listen(3000,()=>{
+    server.listen(port,()=>{
         console.log('server is listening');
     })
 
