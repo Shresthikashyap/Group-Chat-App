16ab0922e97ed01b6c23563757fa2270bc307cc7
Shresthikashyap <122147931+Shresthikashyap@users.noreply.github.com>
2024-10-17 23:56:05 +0530

    api config

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
+//app.js
+
 const express = require('express');
 const bodyParser = require('body-parser'); 
 const sequelize = require('./util/database');
 const http = require('http');
-const path = require('path');
 const socketio = require('socket.io');
 const cron = require('node-cron');
 
@@ -35,17 +36,16 @@ app.use(cors({
     origin:"*",
 })); 
 
-app.use(express.static(path.join(__dirname, 'public')));
-
-// app.use(express.static('public', { 
-//     dotfiles: 'ignore', 
-//     index: false,
-//     extensions: ['html', 'htm'] 
-//   }));
-
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/login.html'); // Adjust the path if your file is located elsewhere
+app.get('/api/config', (req, res) => {
+    res.json({ apiUrl: process.env.API_URL || 'http://localhost:3000' });
 });
+
+app.use(express.static('public', { 
+    dotfiles: 'ignore', 
+    index: false,
+    extensions: ['html', 'htm'] 
+  }));
+
   
 app.use('/user',userRoutes);
 app.use('/message',chatRoutes);
