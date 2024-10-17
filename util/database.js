6d701d195bb6dca3746ad71c091fb92d7c9745cd
Shresthikashyap <122147931+Shresthikashyap@users.noreply.github.com>
2024-10-17 20:58:07 +0530

    Update database.js

--- util/database.js
+++ util/database.js
@@ -1,11 +1,19 @@
 const Sequelize = require('sequelize');
 
 require('dotenv').config();
-console.log( 'confirmation ',process.env.DB_NAME , process.env.DB_USERNAME , process.env.DB_PASSWORD, process.env.DB_HOST)
 
-const sequelize = new Sequelize( process.env.DB_NAME , process.env.DB_USERNAME , process.env.DB_PASSWORD ,{
-    dialect: 'mysql',
-    host: process.env.DB_HOST
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+  dialect: 'mysql',
+  host: process.env.DB_HOST,
+  dialectModule: require('mysql2')  // Ensure mysql2 is explicitly used here
 });
+
+sequelize.authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
 
 module.exports = sequelize;
