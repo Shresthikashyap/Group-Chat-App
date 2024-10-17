6dfbbadb769765f8231dfda7e97fb8498228e378
Shresthikashyap <122147931+Shresthikashyap@users.noreply.github.com>
2024-10-17 14:37:30 +0530

    port setting

--- util/database.js
+++ util/database.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 
 const sequelize = new Sequelize( process.env.DB_NAME , process.env.DB_USERNAME , process.env.DB_PASSWORD ,{
     dialect: 'mysql',
+    port: 3306,
     host: process.env.DB_HOST
 });
 
