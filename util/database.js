8439e8d5c963a84c9a30bcf1022508f09e9f579c
Shresthikashyap <122147931+Shresthikashyap@users.noreply.github.com>
2024-10-17 20:51:23 +0530

    environment variables confirmation in production

--- util/database.js
+++ util/database.js
@@ -1,10 +1,10 @@
 const Sequelize = require('sequelize');
 
 require('dotenv').config();
+console.log( 'confirmation ',process.env.DB_NAME , process.env.DB_USERNAME , process.env.DB_PASSWORD, process.env.DB_HOST)
 
 const sequelize = new Sequelize( process.env.DB_NAME , process.env.DB_USERNAME , process.env.DB_PASSWORD ,{
     dialect: 'mysql',
-    port: 3306,
     host: process.env.DB_HOST
 });
 
