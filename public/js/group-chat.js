9e59c766ab1f59e9da73a28f093dbca17f287188
Shresthikashyap <122147931+Shresthikashyap@users.noreply.github.com>
2024-10-19 18:20:54 +0530

    minor fixes

--- public/js/group-chat.js
+++ public/js/group-chat.js
@@ -5,8 +5,10 @@ async function getApiUrl() {
   return data.apiUrl;
 };
 
-const socket = io({ path: '/socket.io',
-    transports: ['websocket', 'polling']  // Set these explicitly});  // Match the server's transport methods
+const socket = io({
+    path: '/socket.io',
+    transports: ['websocket', 'polling']  // Set these explicitly
+});
 
 // Listen for a 'connect' event when the WebSocket connection is established
 socket.on('connect', ()=>{
